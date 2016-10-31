ca0096f9e6b798cd9c7134e74e926756c454c798
Brit Butler <redline6561@gmail.com>
2016-10-31 10:59:04 -0400

    Add answers for normal mode.

--- src/js/constructors/dog.js
+++ src/js/constructors/dog.js
@@ -1,4 +1,16 @@
-function Dog () {
+function Dog (data) {
+  data = data || {};
+  this.status = "normal";
+  this.color = data.color;
+  this.hungry = data.hungry !== undefined ? data.hungry : true;
+  // data.hungry = false?
+  // this.hungry = false || true;
+  // this.hungry = data.hungry || true;
+  // if (data.hungry !== undefined) {
+  //   this.hungry = data.hungry;
+  // } else {
+  //   this.hungry = true;
+  // }
 }
 
 export { Dog };
--- src/js/constructors/person.js
+++ src/js/constructors/person.js
@@ -1,4 +1,14 @@
-function Person () {
+function Person (options) {
+  options = options || {};
+  this.cool = options.cool !== undefined ? options.cool : false;
+};
+
+Person.prototype.pet = function (dog) {
+  dog.status = 'happy';
+};
+
+Person.prototype.feed = function (dog) {
+  dog.hungry = false;
 };
 
 export { Person };

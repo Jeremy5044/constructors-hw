0cf734d46e353c978b14ff9dae3446b6912875db
Jeremy Springer <Jeremyspringer50@gmail.com>
2016-10-31 16:46:03 -0400

    added some sorce files

--- src/js/constructors/dog.js
+++ src/js/constructors/dog.js
@@ -1,4 +1,15 @@
-function Dog () {
-}
+function Dog (selections) {
+  const selct = selections || {};
+  this.status = selct.status || 'normal';
+  this.color = selct.color || 'black';
+
+  if (selct.hungry === false){
+      this.hungry = false;
+    } else {
+      this.hungry = true;
+    };
+
+};
 
 export { Dog };
+
--- src/js/constructors/person.js
+++ src/js/constructors/person.js
@@ -1,4 +1,19 @@
-function Person () {
+function Person (selections) {
+
+  const selct = selections || {};
+  
+  if ( selct.cool === true) {
+    
+    this.cool = true;
+  } else {
+
+   this.cool = false}
+};
+  Person.prototype.feed = function (data) {
+    return data.hungry = false;
+  }
+  Person.prototype.pet = function (data) {
+  return data.status = 'happy';
 };
 
 export { Person };
